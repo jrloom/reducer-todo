0948861fa5a5f30683effb0761a5d1ad303ec619
Jason Loomis <jason@jrloomis.com>
2019-09-20 00:32:22 -0400

    setup basic structure

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Todo from "./components/Todo";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div>div!</div>
+      <div>
+        <Todo />
+      </div>
     </div>
   );
 }
---
+++ todo/src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form";
+import List from "./List";
+
+const Todo = () => {
+  return (
+    <div>
+      <Form />
+      <List />
+    </div>
+  );
+};
+
+export default Todo;
