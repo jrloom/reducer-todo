328978a4a7ddc7e7aef11bc63ab03437f97fa05a
Jason Loomis <jason@jrloomis.com>
2019-09-21 09:12:22 -0400

    imports and functions written

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,61 @@
-import React from "react";
-import Todo from "./components/Todo";
+import React, { useReducer, useState } from "react";
+import { initialState, taskReducer } from "./reducers/taskReducers";
+import { TaskContext } from "./contexts/TaskContext";
+import Form from "./components/Form";
+import List from "./components/List";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [state, dispatch] = useReducer(taskReducer, initialState);
+  const [task, setTask] = useState("");
+
+  const handleChange = event => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setTask("");
+    dispatch({
+      type: "ADD_TASK",
+      payload: task
+    });
+  };
+
+  const completeTask = task => {
+    dispatch({
+      type: "COMPLETE",
+      payload: task.id
+    });
+  };
+
+  const clearComplete = event => {
+    event.preventDefault();
+    dispatch({ type: "CLEAR_COMPLETE" });
+  };
+
   return (
     <div>
-      <div>
-        <Todo />
-      </div>
+      <TaskContext.Provider
+        value={{
+          task,
+          handleChange,
+          handleSubmit,
+          clearComplete
+        }}
+      >
+        <Form />
+      </TaskContext.Provider>
+      <TaskContext.Provider
+        value={{
+          state,
+          completeTask
+        }}
+      >
+        <List />
+      </TaskContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
