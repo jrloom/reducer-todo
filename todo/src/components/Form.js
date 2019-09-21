a6ff9b83c5c72e4053cb58aad05fae0e27896768
Jason Loomis <jason@jrloomis.com>
2019-09-21 09:11:25 -0400

    form written

--- todo/src/components/Form.js
+++ todo/src/components/Form.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TaskContext } from "../contexts/TaskContext";
 
 const Form = () => {
-  return <div>Todo Form</div>;
+  const form = useContext(TaskContext);
+
+  return (
+    <div>
+      <form onSubmit={event => form.handleSubmit(event)}>
+        <input type="text" value={form.task} onChange={event => form.handleChange(event)} />
+        <button type="submit">Add Task</button>
+        <button type="button" onClick={event => form.clearComplete(event)}>
+          Clear Completed
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
